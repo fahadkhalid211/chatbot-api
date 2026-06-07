18545721759b004f534255207c60a6ad85b837b5
Fahad Khalid <94105504+fahadkhalid211@users.noreply.github.com>
2026-06-08 07:47:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # IMPORTANT for WordPress
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 app = FastAPI()
 
 app.add_middleware(
